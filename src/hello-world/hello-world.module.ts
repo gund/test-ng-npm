279533adff3e5fd290a5a859a399e6bb92830cac
gund <malkevich.alex@gmail.com>
2017-09-02 00:39:34 +0400

    docs(demo): Add demo app

--- src/hello-world/hello-world.module.ts
+++ src/hello-world/hello-world.module.ts
@@ -6,6 +6,7 @@ import { HelloWorldComponent } from './hello-world.component';
   imports: [
     CommonModule
   ],
-  declarations: [HelloWorldComponent]
+  declarations: [HelloWorldComponent],
+  exports: [HelloWorldComponent],
 })
 export class HelloWorldModule { }
